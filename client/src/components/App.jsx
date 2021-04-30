49043272f82503feb34aea84bfcd1276dfcf273a
Jordan Beaird <jordan.beaird@gmail.com>
2021-04-29 19:03:18 -0700

    Created barebones structure for react app with express server and MongoDB database

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Hi from React!
+      </div>
+    )
+  }
+}
+
+export default App;
